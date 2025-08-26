a15c7239def7340be76e56bc495b99dbc4148717
NunSaraam <yjc22950037@gmail.com>
2025-08-26 18:22:29 +0900

    [Feat] 캡슐 랜덤 생성

---
+++ Assets/Scripts/ClawMachine/GenerateCapsule.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateCapsule : MonoBehaviour
+{
+    public GameObject[] capsulePrefab;
+    public Transform generateArea;
+    public Vector3 areaSize = new Vector3 (6, 2, 6);
+
+    private int generateCount = 10;
+
+    private void Start()
+    {
+        if (capsulePrefab == null)
+        {
+            Debug.Log("ƒ∏Ω∂ «¡∏Æ∆’¿Ã ¡∏¿Á«œ¡ˆ æ Ω¿¥œ¥Ÿ!");
+        }
+
+        RandomGenerate();
+    }
+
+    void RandomGenerate()
+    {
+        for (int i = 0; i < generateCount; i++)
+        {
+            Vector3 randomPos = new Vector3(
+                Random.Range(-areaSize.x, areaSize.x),
+                Random.Range(-areaSize.y, areaSize.y),
+                Random.Range(-areaSize.z, areaSize.z)
+            );
+            
+            Vector3 spawnPos = generateArea.position + generateArea.rotation * randomPos;
+
+            GameObject prefab = capsulePrefab[Random.Range(0, capsulePrefab.Length)];
+
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
