39b6a30d485c2a3626648292e8188022772b1f73
alphapo040511 <alphapo05@gmail.com>
2025-09-03 22:49:02 +0900

    [Fix] 뽑기 머신 범위나, 물리 문제 해결
    추가로 배경 변경

--- Assets/Scripts/ClawMachine/GenerateCapsule.cs
+++ Assets/Scripts/ClawMachine/GenerateCapsule.cs
@@ -8,7 +8,8 @@ public class GenerateCapsule : MonoBehaviour
     public Transform generateArea;
     public Vector3 areaSize = new Vector3 (6, 2, 6);
 
-    private int generateCount = 10;
+    [Range(0, 100)]
+    public int generateCount = 10;
 
     private void Start()
     {
@@ -30,11 +31,32 @@ public class GenerateCapsule : MonoBehaviour
                 Random.Range(-areaSize.z, areaSize.z)
             );
             
-            Vector3 spawnPos = generateArea.position + generateArea.rotation * randomPos;
+            if(randomPos.x <= - areaSize.x + 4f && randomPos.z >= areaSize.z - 4f)
+            {
+                randomPos.x = Random.Range(-areaSize.x + 4f, areaSize.x);
+            }
+
+            Vector3 spawnPos = generateArea.position + randomPos;
 
             GameObject prefab = capsulePrefab[Random.Range(0, capsulePrefab.Length)];
 
             Instantiate(prefab, spawnPos, Quaternion.identity);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        // 이전 행렬 저장
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.color = Color.blue;
+
+        // 오브젝트의 로컬 기준으로 변환
+        Gizmos.matrix = generateArea.localToWorldMatrix;
+
+        // 로컬 기준 (0,0,0) 위치에 박스 그리기
+        Gizmos.DrawWireCube(Vector3.zero, areaSize * 2);
+
+        // 행렬 복구
+        Gizmos.matrix = oldMatrix;
     }
 }
